8801024d5de91091ac64683d0131a02bfaee2842
T.Watanabe <watanabe@colorfulcompany.co.jp>
2020-01-07 18:31:12 +0900

    prepare emulator for repository spec

--- test/repository.spec.js
+++ test/repository.spec.js
@@ -1,7 +1,10 @@
-/* global describe, it */
+/* global describe, it, beforeEach, afterEach */
 
+const Emulator = require('./emulator')
+const sleep = require('sleep-promise')
 const assert = require('power-assert')
 
+const RepositoryCreator = require('repository-creator')
 const Repository = require('repository')
 
 describe('Repository', () => {
@@ -11,6 +14,20 @@ describe('Repository', () => {
         () => new Repository({}),
         { name: 'CollectionInvalid' }
       )
+    })
+  })
+
+  describe('regular initialized', function () {
+    this.timeout(20000)
+    let emu, repo // eslint-disable-line no-unused-vars
+
+    beforeEach(async () => {
+      emu = Emulator.invoke('127.0.0.1', 9876)
+      await sleep(2500)
+      repo = RepositoryCreator.create('test-collection', 'test-project')
+    })
+    afterEach(async () => {
+      await emu.kill()
     })
   })
 })
