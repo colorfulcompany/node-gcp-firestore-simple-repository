641f950482528660c39e544d51a6ab4967a0a4d8
T.Watanabe <watanabe@colorfulcompany.co.jp>
2020-01-11 00:43:48 +0900

    add index.js and description

---
+++ index.js
@@ -0,0 +1,11 @@
+const FirestoreCreator = require('./src/firestore-creator')
+const RepositoryCreator = require('./src/repository-creator')
+const Repository = require('./src/repository')
+const EmulatorController = require('./src/emulator-controller')
+
+module.exports = {
+  FirestoreCreator,
+  RepositoryCreator,
+  Repository,
+  EmulatorController
+}
